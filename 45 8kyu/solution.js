787789161b059a4b48e07d43bf53e8aac6f566d5
Isaiah Lowe-Brown <isaiahlowebrown@gmail.com>
2022-11-11 09:28:32 -0500

    8kyu Sentence Smash

---
+++ 45 8kyu/solution.js
@@ -0,0 +1,13 @@
+function smash (words) {
+    return words.join(" ")
+ };
+
+//Sentence Smash
+//Write a function that takes an array of words 
+//smashes them together into a sentence 
+//returns the sentence. 
+//You can ignore any need to sanitize words or add punctuation, 
+//but you should add spaces between each word. 
+//Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+//https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript
