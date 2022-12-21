f4bcea573ba3f9638e072ff5c5a057016a804ef8
Isaiah Lowe-Brown <isaiahlowebrown@gmail.com>
2022-12-20 23:16:28 -0500

    8 kyu Are You Playing Banjo?

---
+++ 72 8kyu/solution.js
@@ -0,0 +1,5 @@
+function areYouPlayingBanjo(name) {
+    // Implement me
+    return name.slice(0,1) === "R" || name.slice(0,1) === "r" ? name + " plays banjo" : name + " does not play banjo"
+  
+  }
